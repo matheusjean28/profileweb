7752a88d69d55ee7e0f3a6042258cfc95d1f3af1
matheusjean28 <matheusjean28@gmail.com>
2023-07-23 10:02:21 -0300

    <Feat> <Frameworks.jsx> : <Adicionado alguns elementos>

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -5,25 +5,25 @@ function Frameworks() {
     <>
       <div className="stackConteiner">
         <h2> TECNOLOGIAS</h2>
-
         <ul>
           <li className="StackLiStyle ">
             <img className="rotate" src="./src/icons/atom.png" alt="" />
             <p>ReactJs</p>
           </li>
           <li className="StackLiStyle">
-          <img src="./src/icons/js-file.png" alt="" />
-            <p>teste</p>
+            <img src="./src/icons/js-file.png" alt="" />
+            <p>JavaScript</p>
           </li>
           <li className="StackLiStyle">
-          <img src="./src/icons/mysql.png" alt="" />
-
-            <p>teste</p>
+            <img src="./src/icons/mysql.png" alt="" />
+            <p>MySQL</p>
+          </li>
+          <li className="StackLiStyle ">
+            <img  src="./src/icons/csharp.png" alt="" />
+            <p>C-Shap</p>
           </li>
           <li className="StackLiStyle">
-            <p>teste</p>
-          </li>
-          <li className="StackLiStyle">
+          <img  src="./src/icons/nodejss.png" alt="" />
             <p>teste</p>
           </li>
           <li className="StackLiStyle">
